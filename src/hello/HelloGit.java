0702c7e9aaf307bfc3f8da645745042ec272edae
Kanject <869307869@qq.com>
2018-03-28 15:54:40 +0800

    Second commit: add "Welcome! Nice to meet you."

--- src/hello/HelloGit.java
+++ src/hello/HelloGit.java
@@ -1,11 +1,12 @@
 package hello;
 
 /**
- * @description: 上传到Git的测试代码
+ * @description: 上传到Git的测试代码2.0
  * @author: Kanject
  */
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git, I come from IDEA.");
+        System.out.println("Welcome! Nice to meet you.");
     }
 }
