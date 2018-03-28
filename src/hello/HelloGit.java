ce5928cdcc52b1c75a8be832ef41b2618faf6002
Kanject <869307869@qq.com>
2018-03-28 09:40:10 +0800

    First commit.

---
+++ src/hello/HelloGit.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * @description: 上传到Git的测试代码
+ * @author: Kanject
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git, I come from IDEA.");
+    }
+}
